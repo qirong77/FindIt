f71a2a4545a9fe6e2a3e55188a2b139b2663971d
linqirong <609413692@qq.com>
2023-03-26 21:03:16 +0800

    feat:完成render基础部分

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,82 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { IFile } from 'src/common/types'
+import { GET_SEARCH_RESULTS, GOOGLE, OPEN_FILE, SET_WINDOW_SIZE } from '../../common/const'
+import { AppIcon, FileIcon, GoogleIcon, SearchIcon } from './icons'
+let timer
 export const App = () => {
+  const [search, setSearch] = useState('')
+  const [files, setFiles] = useState<IFile[]>([])
+  const [active, setActive] = useState(0)
+  const [status, setStatus] = useState(0)
+  const inputRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    if (timer || status !== 0) return
+    timer = window.setTimeout(() => {
+      window.api.interProcess(GET_SEARCH_RESULTS, search).then((files) => {
+        setFiles(files.slice(0, 3))
+        setActive(0)
+      })
+      timer = null
+    }, 800)
+  }, [search])
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
   return (
-    <div>
-      <h1 className="text-cyan-400">app</h1>
+    <div className="container min-w-sm">
+      <div className="flex pl-[14px]  items-center h-[50px]  border-1 border-slate-400">
+        {status === 0 ? <SearchIcon /> : <GoogleIcon />}
+        <input
+          ref={inputRef}
+          onKeyDown={handleKeyDown}
+          className="w-5/6 mx-4 pl-[4px] h-3/5 text-lg"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {files.map((file, i) => (
+        <ul className="pl-[10px] my-[6px]" key={i}>
+          <li
+            className={`flex items-center  ${
+              file.fileName === files[active]?.fileName ? 'active-li' : ''
+            }`}
+            key={file.fileName}
+          >
+            {file.isApp ? <AppIcon /> : <FileIcon />}
+            <span>{file.fileName}</span>
+          </li>
+        </ul>
+      ))}
     </div>
   )
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'ArrowUp') {
+      e.metaKey ? setActive(0) : setActive(active - 1 < 0 ? files.length - 1 : active - 1)
+    }
+    if (e.key === 'ArrowDown') {
+      e.metaKey
+        ? setActive(files.length - 1)
+        : setActive(active + 1 > files.length - 1 ? 0 : active + 1)
+    }
+    console.log(e.key)
+    if (e.key === 'Tab') {
+      status === 0 ? setStatus(1) : setStatus(0)
+      setFiles([])
+      window.api.sendToMain(SET_WINDOW_SIZE, 50)
+    }
+    if (e.key === 'Enter') {
+      if (status === 0 && search) {
+        window.api.sendToMain(OPEN_FILE, {
+          ...files[active],
+          search
+        })
+      }
+      if (status === 1) {
+        window.api.sendToMain(GOOGLE, search)
+      }
+    }
+  }
 }
---
+++ src/renderer/src/icons/index.tsx
@@ -0,0 +1,51 @@
+export const AppIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="mx-[6px]"
+      viewBox="0 0 16 16"
+    >
+      <path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+    </svg>
+  )
+  export const SearchIcon = () => (
+    <svg
+      className="mx-[6px]"
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+    </svg>
+  )
+  
+  export const FileIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      className="mx-[6px]"
+    >
+      <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
+      <path d="M8.646 6.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 9 8.646 7.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 9l1.647-1.646a.5.5 0 0 0 0-.708z" />
+    </svg>
+  )
+  export const GoogleIcon = () => (
+    <svg
+    className="mx-[6px]"
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      viewBox="0 0 16 16"
+    >
+      <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+    </svg>
+  )
+  
